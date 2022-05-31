195927137dc89b931bf47989c169d98e1072d431
Dom <dominicd7@hotmail.com>
2022-05-31 14:32:22 +0200

    Move attractor network to its own class

---
+++ attractor_network.py
@@ -0,0 +1,92 @@
+import brian2 as b2
+
+# Diehl and Cook neuron model from https://github.com/sdpenguin/Brian2STDPMNIST/blob/2d935d0a98b6c94cfbc1cb8304f16a578a57342b/Diehl%26Cook_spiking_MNIST_Brian2.py#L204
+neuron_eqs_e = '''
+	dv/dt = ((v_rest_e - v) + (I_synE+I_synI) / nS) / (100*ms)  : volt (unless refractory)
+	I_synE = ge * nS *         -v                           : amp
+	I_synI = gi * nS * (-100.*mV-v)                          : amp
+	dge/dt = -ge/(tau_e)                                   : 1
+	dgi/dt = -gi/(2.0*ms)                                  : 1
+	theta      :volt
+'''
+
+neuron_eqs_i = '''
+	dv/dt = ((v_rest_i - v) + (I_synE+I_synI) / nS) / (10*ms)  : volt (unless refractory)
+	I_synE = ge * nS *         -v                           : amp
+	I_synI = gi * nS * (-85.*mV-v)                          : amp
+	dge/dt = -ge/(1.0*ms)                                   : 1
+	dgi/dt = -gi/(2.0*ms)                                  : 1
+'''
+
+synapse_eqs_e = '''
+w = weight_ee*exp(-(i_pre-i_post)**2 / (2*stdev_ee**2)) : 1
+'''
+
+default_params = {
+	'v_rest_e': -65. * b2.mV,
+	'v_rest_i': -60. * b2.mV,
+	'v_reset_e': -65. * b2.mV,
+	'v_reset_i': -45. * b2.mV,
+	'v_thresh_e': -52. * b2.mV,
+	'v_thresh_i': -40. * b2.mV,
+	'offset': 20.0*b2.mV,
+	'refrac_e': 5. * b2.ms,
+	'refrac_i': 2. * b2.ms,
+	'tau_e': 5.0*b2.ms,
+	'weight_ee': 15,
+	'stdev_ee': 1.5,
+	'weight_ei': 2,
+	'weight_ie': 50,
+	'weight_input': 20,
+}
+
+
+class AttractorNetwork():
+	'''
+	Attractor network with E and I neurons with synaptic current dynamics
+	'''
+	def __init__(self, num_e=16, num_i=1) -> None:
+		self.num_e = num_e
+		self.num_i = num_i
+
+		v_thresh_e_str = 'v>v_thresh_e'
+		v_thresh_i_str = 'v>v_thresh_i'
+		v_reset_i_str = 'v=v_reset_i'
+		scr_e = 'v = v_reset_e; timer = 0*ms'
+
+		self.params = default_params.copy()
+
+		self.network = b2.Network()
+		neuron_group_e = b2.NeuronGroup(self.num_e, neuron_eqs_e, threshold=v_thresh_e_str, refractory='refrac_e', reset=scr_e, method='euler')
+		neuron_group_i = b2.NeuronGroup(self.num_i, neuron_eqs_i, threshold=v_thresh_i_str, refractory='refrac_i', reset=v_reset_i_str, method='euler')
+		neuron_group_e.v = self.params['v_rest_e']
+		neuron_group_i.v = self.params['v_rest_i']
+
+		ee_connection = b2.Synapses(neuron_group_e, neuron_group_e, synapse_eqs_e, on_pre='ge_post += w')
+		ee_connection.connect(True)
+
+		ei_connection = b2.Synapses(neuron_group_e, neuron_group_i, 'w = weight_ei : 1', on_pre='ge_post += w')
+		ei_connection.connect(True)
+
+		ie_connection = b2.Synapses(neuron_group_i, neuron_group_e, 'w = weight_ie : 1', on_pre='gi_post += w')
+		ie_connection.connect(True)
+
+		self.neuron_group_input = b2.PoissonGroup(self.num_e, 0*b2.Hz)
+		input_connection = b2.Synapses(self.neuron_group_input, neuron_group_e, 'w = weight_input : 1', on_pre='ge_post += w')
+		input_connection.connect('i==j')
+
+		self.monitor_e = b2.SpikeMonitor(neuron_group_e)
+		self.monitor_i = b2.SpikeMonitor(neuron_group_i)
+		self.monitor_input = b2.SpikeMonitor(self.neuron_group_input)
+
+		self.network.add([neuron_group_e, neuron_group_i, self.neuron_group_input, ee_connection, ei_connection, ie_connection, input_connection, self.monitor_e, self.monitor_i, self.monitor_input])
+
+		self.network.store()
+	
+	def reset(self):
+		self.params = default_params.copy()
+		self.network.restore()
+
+	def run_with_inputs(self, duration, input_rates):
+		self.neuron_group_input.rates = input_rates
+		self.network.run(duration, namespace=self.params)
--- basic_attractor.py
+++ basic_attractor.py
@@ -1,68 +1,7 @@
+import numpy as np
 import brian2 as b2
 import matplotlib.pyplot as plt
-import numpy as np
-
-# b2.set_device('cpp_standalone', build_on_run=False)
-
-b2.start_scope()
-
-# Diehl and Cook neuron model from https://github.com/sdpenguin/Brian2STDPMNIST/blob/2d935d0a98b6c94cfbc1cb8304f16a578a57342b/Diehl%26Cook_spiking_MNIST_Brian2.py#L204
-n_e = 16
-n_i = 1
-v_rest_e = -65. * b2.mV
-v_rest_i = -60. * b2.mV
-v_reset_e = -65. * b2.mV
-v_reset_i = -45. * b2.mV
-v_thresh_e = -52. * b2.mV
-v_thresh_i = -40. * b2.mV
-refrac_e = 5. * b2.ms
-refrac_i = 2. * b2.ms
-
-scr_e = 'v = v_reset_e; timer = 0*ms'
-offset = 20.0*b2.mV
-# v_thresh_e_str = '(v>(theta - offset + v_thresh_e)) and (timer>refrac_e)'
-v_thresh_e_str = 'v>v_thresh_e'
-v_thresh_i_str = 'v>v_thresh_i'
-v_reset_i_str = 'v=v_reset_i'
-
-
-neuron_eqs_e = '''
-dv/dt = ((v_rest_e - v) + (I_synE+I_synI) / nS) / (100*ms)  : volt (unless refractory)
-I_synE = ge * nS *         -v                           : amp
-I_synI = gi * nS * (-100.*mV-v)                          : amp
-dge/dt = -ge/(5.0*ms)                                   : 1
-dgi/dt = -gi/(2.0*ms)                                  : 1
-theta      :volt
-'''
-
-neuron_eqs_i = '''
-dv/dt = ((v_rest_i - v) + (I_synE+I_synI) / nS) / (10*ms)  : volt (unless refractory)
-I_synE = ge * nS *         -v                           : amp
-I_synI = gi * nS * (-85.*mV-v)                          : amp
-dge/dt = -ge/(1.0*ms)                                   : 1
-dgi/dt = -gi/(2.0*ms)                                  : 1
-'''
-
-neuron_group_e = b2.NeuronGroup(n_e, neuron_eqs_e, threshold=v_thresh_e_str, refractory=refrac_e, reset=scr_e, method='euler')
-neuron_group_i = b2.NeuronGroup(n_i, neuron_eqs_i, threshold=v_thresh_i_str, refractory=refrac_i, reset=v_reset_i_str, method='euler')
-neuron_group_e.v = v_rest_e
-neuron_group_i.v = v_rest_i
-
-monitor_e = b2.SpikeMonitor(neuron_group_e)
-monitor_i = b2.SpikeMonitor(neuron_group_i)
-voltage_e = b2.StateMonitor(neuron_group_e, 'v', record=True)
-
-ee_connection = b2.Synapses(neuron_group_e, neuron_group_e, 'w : 1', on_pre='ge_post += w')
-ee_connection.connect(True)
-ee_connection.w = '15.0*exp(-(i_pre-i_post)**2 / (2*1.5**2))'
-
-ei_connection = b2.Synapses(neuron_group_e, neuron_group_i, 'w : 1', on_pre='ge_post += w')
-ei_connection.connect(True)
-ei_connection.w = '2.0'
-
-ie_connection = b2.Synapses(neuron_group_i, neuron_group_e, 'w : 1', on_pre='gi_post += w')
-ie_connection.connect(True)
-ie_connection.w = '50.0'
+import attractor_network
 
 plot_weights = False
 if plot_weights:
@@ -72,39 +11,42 @@ if plot_weights:
 	plt.imshow(weights)
 	plt.show()
 
-neuron_group_input = b2.PoissonGroup(n_e, 0*b2.Hz)
-input_connection = b2.Synapses(neuron_group_input, neuron_group_e, 'w : 1', on_pre='ge_post += w')
-input_connection.connect('i==j')
-input_connection.w = '20.0'
-monitor_input = b2.SpikeMonitor(neuron_group_input)
+
+network = attractor_network.AttractorNetwork()
+
+
+# network.run_with_inputs(duration, [100*b2.Hz if 1<=i<=4 else 20*b2.Hz for i in range(16)])
+# network.run_with_inputs(duration, 0)
+
+# network.run_with_inputs(duration, [100*b2.Hz if 1<=i<=4 else 20*b2.Hz for i in range(16)])
+
+def plot_results(network):
+	fig, axs = plt.subplots(3, 1, figsize=(12,4), sharex=True)
+	assert(isinstance(axs, np.ndarray))
+	axs[0].plot(network.monitor_e.t/b2.ms, network.monitor_e.i[:], '.b')
+	axs[0].set_ylabel('Neuron index')
+	axs[0].set_ylim([0,network.num_e])
+	axs[0].set_title('E spikes')
+	axs[1].plot(network.monitor_i.t/b2.ms, network.monitor_i.i[:], '.r')
+	axs[1].set_ylabel('Neuron index')
+	axs[1].set_title('I spikes')
+	axs[2].plot(network.monitor_input.t/b2.ms, network.monitor_input.i[:], '.k')
+	axs[2].set_xlabel('Time (ms)')
+	axs[2].set_ylabel('Neuron index')
+	axs[2].set_title('Input spikes')
+	plt.tight_layout()
+	plt.show()
 
 duration = 200*b2.ms
+network.run_with_inputs(duration, [100*b2.Hz if 8<=i<=10 else 70*b2.Hz for i in range(network.num_e)])
+plot_results(network)
 
-neuron_group_input.rates = [100*b2.Hz if 8<=i<=10 else 70*b2.Hz for i in range(n_e)]
-b2.run(duration)
-neuron_group_input.rates = [100*b2.Hz if 1<=i<=4 else 20*b2.Hz for i in range(n_e)]
-b2.run(duration)
-neuron_group_input.rates = 0
-b2.run(duration)
+network.reset()
+network.params['weight_ee'] = 50
+network.run_with_inputs(duration, [100*b2.Hz if 8<=i<=10 else 70*b2.Hz for i in range(network.num_e)])
+plot_results(network)
 
-# b2.device.build(directory='output', compile=True, run=True, debug=False)
-
-fig, axs = plt.subplots(3, 1, figsize=(12,4), sharex=True)
-assert(isinstance(axs, np.ndarray))
-axs[0].plot(monitor_e.t/b2.ms, monitor_e.i[:], '.b')
-axs[0].set_ylabel('Neuron index')
-axs[0].set_ylim([0,n_e])
-axs[0].set_title('E spikes')
-axs[1].plot(monitor_i.t/b2.ms, monitor_i.i[:], '.r')
-axs[1].set_ylabel('Neuron index')
-axs[1].set_title('I spikes')
-# plt.plot(voltage_e.t/b2.ms, voltage_e.v[:].T)
-# plt.set_xlabel('Time (ms)')
-# plt.set_ylabel('Neuron index')
-# plt.set_title('E voltage')
-axs[2].plot(monitor_input.t/b2.ms, monitor_input.i[:], '.k')
-axs[2].set_xlabel('Time (ms)')
-axs[2].set_ylabel('Neuron index')
-axs[2].set_title('Input spikes')
-plt.tight_layout()
-plt.show()
+network.reset()
+network.params['weight_ie'] = 100
+network.run_with_inputs(duration, [100*b2.Hz if 8<=i<=10 else 70*b2.Hz for i in range(16)])
+plot_results(network)
